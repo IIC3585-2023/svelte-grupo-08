c8917fda74e5979de45d4e7d5234c42d4d070251
Ana Sofia Marin Alexandre <ana.marn@uc.cl>
2023-06-06 19:24:57 -0400

    first commit home page

---
+++ README.txt
@@ -0,0 +1,3 @@
+1: cd myapp
+2: npm install (or pnpm install, etc) 
+3: npm run dev -- --open
---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { page } from '$app/stores';
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<h1>Trump Card Pokemon</h1>
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
